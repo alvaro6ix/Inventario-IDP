50aaef91ad34f5643c551395decfa9f93872a184
alvaro6ix <alvaro69aldma@gmail.com>
2025-07-12 10:43:46 -0600

    Elimina ruta temporal /migrar por seguridad

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InventarioController;
-use Illuminate\Support\Facades\Artisan;
-
-Route::get('/migrar', function () {
-    try {
-        Artisan::call('migrate', ['--force' => true]);
-        return '✅ Migraciones ejecutadas correctamente.';
-    } catch (Exception $e) {
-        return '❌ Error al ejecutar migraciones: ' . $e->getMessage();
-    }
-});
 
 
 // Ruta para la página de bienvenida (puedes cambiarla si es necesario)
