dd848afcb0e73a2f1cdaa358646c5f0eada788c6
alvaro6ix <alvaro69aldma@gmail.com>
2025-07-12 21:58:38 -0600

    Arreglar build y dependencias frontend

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InventarioController;
-Route::get('/ver-logs', function () {
-    $logPath = storage_path('logs/laravel.log');
-    if (!file_exists($logPath)) {
-        return 'No se encontró el archivo de logs.';
-    }
-    $logs = file_get_contents($logPath);
-    return nl2br(e($logs));
-});
-Route::get('inventarios/exportExcel', function() {
-    return 'Exportar Excel - ruta temporal.';
-})->name('inventarios.exportExcel');
-
 
 // Ruta para la página de bienvenida (puedes cambiarla si es necesario)
 Route::get('/', function () {
