8e0de3d5ee56a70d4fabf81edbde2fb66bc9411e
alvaro6ix <alvaro69aldma@gmail.com>
2025-07-13 13:54:39 -0600

    Corregir ruta de vista principal para evitar error 500

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,10 @@ use App\Http\Controllers\InventarioController;
 
 // Ruta para la página de bienvenida (puedes cambiarla si es necesario)
 Route::get('/', function () {
-    return view('welcome'); // O cualquier otra vista principal que tengas
+    return view('layouts.welcome');
+ // O cualquier otra vista principal que tengas
 });
+
 
 // Rutas para el CRUD de inventarios
 Route::resource('inventarios', InventarioController::class);
